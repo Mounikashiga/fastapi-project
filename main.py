a4cf40e659415c108212e2900ade1e4cfee3805b
Mounikashiga <mounikashiga@gmail.com>
2026-03-24 18:30:43 +0530

    Day 1 and Day 2 FastAPI APIs

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to FastAPI"}
 
 @app.get("/hello")
 def hello():
-    return {"message": "Hello Endpoint Working"}
+    return {"message": "Hello Endpoint working"}
+# ======================
+# DAY 2 APIs
+# ======================
+
+@app.get("/greet")
+def greet(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
